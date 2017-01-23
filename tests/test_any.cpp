b773f2cf82af2ba355b8285bd643afc0bad084f5
Michael Lee <mikecovlee@163.com>
2017-01-23 20:55:26 +0800

    改进cov::any
    为cov::any内置了引用计数器以增强性能

---
+++ tests/test_any.cpp
@@ -0,0 +1,27 @@
+#include "../headers/any.hpp"
+#include <iostream>
+static int count=0;
+struct foo{
+    int dat;
+    int mc;
+    foo(){
+        ++count;
+        mc=count;
+        std::cerr<<__func__<<mc<<std::endl;
+    }
+    foo(const foo& obj):dat(obj.dat){
+        ++count;
+        mc=count;
+        std::cerr<<__func__<<mc<<std::endl;
+    }
+    ~foo(){
+        std::cerr<<__func__<<mc<<std::endl;
+    }
+};
+int main()
+{
+    cov::any var=10;
+    var=(10>1);
+    var.const_val<bool>();
+    return 0;
+}
