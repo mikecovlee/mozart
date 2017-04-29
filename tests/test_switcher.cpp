7aa3eb55c1c8118d868bb56d5330e5abc5371f7b
Michael Lee <mikecovlee@163.com>
2017-04-29 16:41:29 +0800

    更新cov::switcher，改善性能

---
+++ tests/test_switcher.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "../headers/switcher.hpp"
+int main()
+{
+	std::string str;
+	std::getline(std::cin,str);
+	CovSwitch(str) {
+		CovCase("Hello") {
+			std::cout<<"Hahaha"<<std::endl;
+		}
+		EndCovCase;
+		CovDefault {
+			std::cout<<"Default"<<std::endl;
+		} EndCovCase;
+	}
+	EndCovSwitch;
+	return 0;
+}
